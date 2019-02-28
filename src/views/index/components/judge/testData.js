774b210457da03ca7761147e56128699fa110602
huangsheng <sheng.huang@aorise.org>
2019-02-28 15:14:52 +0800

    研判-轨迹碰撞-事件侦查

--- src/views/index/components/judge/testData.js
+++ src/views/index/components/judge/testData.js
@@ -1,32 +1,158 @@
 export const testData = {
   sxt: [
     {
-      name: '摄像头111',
-      addr: '长沙市天心区',
+      id: 1,
+      time: '2018-12-11 14:25',
       latitude: 28.093856,
-      longitude: 112.974557
+      longitude: 112.973557,
+      checked: true,
+      playing: false,
+      vUrl: '../../../../assets/video/demo.mp4'
     }, {
-      name: '摄像头222',
-      addr: '创谷广告园',
+      id: 2,
+      time: '2018-12-12 14:25',
       latitude: 28.093804,
-      longitude: 112.975828
+      longitude: 112.976828,
+      checked: true,
+      playing: false,
+      vUrl: '../../../../assets/video/demo.mp4'
     },
     {
-      name: '摄像头333',
-      addr: '长沙市天心区',
+      id: 3,
+      time: '2018-12-13 14:25',
       latitude: 28.093222,
-      longitude: 112.974718
+      longitude: 112.978718,
+      checked: true,
+      playing: false,
+      vUrl: '../../../../assets/video/demo.mp4'
     }, {
-      name: '摄像头444',
-      addr: '创谷广告园',
+      id: 4,
+      time: '2018-12-14 14:25',
       latitude: 28.093537,
-      longitude: 112.975628
+      longitude: 112.981628,
+      checked: true,
+      playing: false,
+      vUrl: '../../../../assets/video/demo.mp4'
     },
     {
-      name: '摄像头555',
-      addr: '长沙市天心区',
+      id: 5,
+      time: '2018-12-15 14:25',
       latitude: 28.094869,
-      longitude: 112.975227
+      longitude: 112.975227,
+      checked: true,
+      playing: false,
+      vUrl: '../../../../assets/video/demo.mp4'
+    },
+  ],
+  zp: [
+    {
+      id: 1,
+      name: '摄像头001',
+      latitude: 28.093856,
+      longitude: 112.973557,
+      times: 1,
+      checked: false,
+      videoList: [
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        }
+      ]
+    }, {
+      id: 2,
+      name: '摄像头002',
+      latitude: 28.093804,
+      longitude: 112.976828,
+      times: 4,
+      checked: false,
+      videoList: [
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: '摄像头003',
+      latitude: 28.093222,
+      longitude: 112.978718,
+      times: 8,
+      checked: false,
+      videoList: [
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          checked: true,
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        },
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        }
+      ]
+    }, {
+      id: 4,
+      name: '摄像头004',
+      latitude: 28.093537,
+      longitude: 112.981628,
+      times: 1,
+      checked: false,
+      videoList: [
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        }
+      ]
+    },
+    {
+      id: 5,
+      name: '摄像头005',
+      latitude: 28.094869,
+      longitude: 112.975227,
+      times: 1,
+      checked: false,
+      videoList: [
+        {
+          time: '2018-12-11 14:25',
+          playing: false,
+        }
+      ]
     },
   ]
 }
