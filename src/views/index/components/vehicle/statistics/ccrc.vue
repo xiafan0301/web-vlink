66d2bdaa6f20ce958cc9bcd293dc09d03b22df11
wxfan <441771878@qq.com>
2019-06-26 18:27:12 +0800

    ll

--- src/views/index/components/vehicle/statistics/ccrc.vue
+++ src/views/index/components/vehicle/statistics/ccrc.vue
@@ -1,17 +1,224 @@
 <template>
-  <div>
-    车辆轨迹分析
+  <div class="ccrc">
+    <div class="ccrc_breadcrumb">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item>车辆侦查</el-breadcrumb-item>
+          <el-breadcrumb-item>初次入城</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+    <div class="ccrc_content">
+      <div class="ccrc_content_left">
+        <div class="kaishi">
+          <span style="display: inline-block; width: 14px; margin-right: 4px; color: #999999">开 始</span>
+          <el-date-picker
+              v-model="value1"
+              style="width: 212px; vertical-align: top"
+              type="datetime"
+              placeholder="选择日期时间">
+          </el-date-picker>
+        </div>
+        <div class="jiesu">
+          <span style="display: inline-block; width: 14px; margin-right: 4px; color: #999999">结 束</span>
+          <el-date-picker
+              v-model="value1"
+              style="width: 212px; vertical-align: top"
+              type="datetime"
+              placeholder="选择日期时间">
+          </el-date-picker>
+        </div>
+        <div class="kakou">
+          <el-select v-model="value1" placeholder="请选择" style="width: 230px">
+            <el-option>kkk</el-option>
+          </el-select>
+        </div>
+        <div class="cpai">
+          <span style="display: inline-block; width: 42px;color: #999999">车牌：</span>
+          <el-radio v-model="value1" label="1"><span style="color: #999999">非</span></el-radio>
+        </div>
+        <div class="kakou">
+          <el-input placeholder="请输入内容" v-model="value1" class="input-with-select">
+            <el-select v-model="value2" slot="prepend" style="width: 62px;" class="selectet" :placeholder="null">
+              <el-option label="箱" value="1"></el-option>
+              <el-option label="订单号" value="2"></el-option>
+              <el-option label="用户电话" value="3"></el-option>
+            </el-select>
+          </el-input>
+        </div>
+        <div class="kakou">
+          <el-button style="width: 110px">重置</el-button>
+          <el-button type="primary" style="width: 110px">统计</el-button>
+        </div>
+      </div>
+      <div class="ccrc_content_right">
+        <div class="ccrc_content_right_content">
+          <div class="ccrc_content_right_table">
+            <el-table
+                :data="tableData"
+                style="width: 100%">
+              <el-table-column
+                  type="index"
+                  prop="date"
+                  label="序号">
+              </el-table-column>
+              <el-table-column
+                  prop="name"
+                  label="车牌号码">
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="卡口名称"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="抓拍时间"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="车道"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="车牌颜色"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="布控库"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="车辆类型"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  prop="address"
+                  label="车速"
+                  show-overflow-tooltip>
+              </el-table-column>
+              <el-table-column
+                  label="操作">
+              </el-table-column>
+            </el-table>
+          </div>
+          <el-pagination
+              class="cum_pagination"
+              @current-change="handleCurrentChange"
+              :current-page.sync="pagination.pageNum"
+              :page-sizes="[100, 200, 300, 400]"
+              :page-size="pagination.pageSize"
+              layout="total, prev, pager, next, jumper"
+              :total="pagination.total">
+          </el-pagination>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      value1: '',
+      value2: '',
+      pagination: { total: 0, pageSize: 10, pageNum: 1 },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   methods: {
+    handleCurrentChange (page) {
+      this.pagination.pageNum = page;
+    },
   }
 }
 </script>
 <style lang="scss" scoped>
+  .ccrc{
+    height: 100%;
+    .ccrc_breadcrumb{
+      background-color: white;
+      padding: 23px 20px;
+      border-bottom: 2px solid #F7F9F9;
+    }
+    .ccrc_content{
+      display: flex;
+      height: calc(100% - 62px);
+      .ccrc_content_left{
+        height: 100%;
+        background-color: white;
+        width: 272px;
+        padding: 20px;
+        box-shadow: 5px 0 10px #E5E7E7;
+        .jiesu{
+          padding-top: 10px;
+        }
+        .kakou{
+          padding-top: 10px;
+        }
+        .cpai{
+          padding-top: 41px;
+        }
+        .selectet{
+          /deep/ .el-input__inner{
+            background-color: white;
+            border: 1px solid #DCDFE6;
+            border-left: 0;
+            border-right: 0;
+          }
+        }
+      }
+      .ccrc_content_right{
+        height: 100%;
+        background-color: #F7F9F9;
+        width: calc(100% - 282px);
+        margin-left: 10px;
+        padding: 20px;
+        overflow: auto;
+        .ccrc_content_right_content{
+          min-height: 100%;
+          background-color: white;
+          box-shadow:5px 0px 16px 0px rgba(169,169,169,0.2);
+          padding: 20px;
+          .cum_pagination{
+            padding-right: 0;
+          }
+        }
+      }
+    }
+  }
 </style>
