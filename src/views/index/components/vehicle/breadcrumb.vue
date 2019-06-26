c86f5fd9f099294f40c9d6a6a6bd866b655bd76f
longjiao.ouyang <longjiao.ouyang@aorise.org>
2019-06-26 17:17:36 +0800

    面包屑

---
+++ src/views/index/components/vehicle/breadcrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vc_breadcrumb">
+    <el-breadcrumb separator=">">
+        <el-breadcrumb-item :to="{ name: 'vehicle' }">车辆侦查</el-breadcrumb-item>
+        <el-breadcrumb-item>{{title}}</el-breadcrumb-item>
+      </el-breadcrumb>
+  </div>
+</template>
+<script>
+export default {
+  props: ['title'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.vc_breadcrumb {
+  height: 50px;
+  padding-left: 20px; padding-top: 17px;
+  border-bottom: 1px solid #f6f6f6;
+  box-shadow: 0 0 5px #ddd;
+}
+</style>
