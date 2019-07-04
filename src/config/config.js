6d0c8ab0f66fb62bda70521acde20acd324b11f4
e <fen.wang@aorise.org>
2019-07-04 14:07:06 +0800

    新建频繁出没接口

--- src/config/config.js
+++ src/config/config.js
@@ -45,9 +45,9 @@ const oDomains = {
     },
     // 研判检索服务
     judge: {
-        // dev: 'https://apidev.aorise.org/vlink-judge/',
-        dev: 'http://10.116.64.127:8082',
-        // dev: 'https://apidev.aorise.org/vlink-judge/',
+        dev: 'https://apidev.aorise.org/vlink-judge/',
+        // dev: 'http://10.116.64.127:8082',
+        // dev: 'http://10.116.64.107:8081',
         test: 'https://apirel.aorise.org/vlink-judge/',
         prod: 'https://apirel.aorise.org/vlink-judge/',
     },
