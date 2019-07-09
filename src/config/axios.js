616fb67618c412f01a52cbb576281b53f30efb41
longjiao.ouyang <longjiao.ouyang@aorise.org>
2019-07-09 08:21:34 +0800

    didi

--- src/config/axios.js
+++ src/config/axios.js
@@ -49,9 +49,9 @@ service.interceptors.response.use(function (response) {
     if (_data.code === '00000000') {
       return _data;
     } else if (_data.code === '10060002') {
-      store.commit('setLoginToken', {
+      /* store.commit('setLoginToken', {
         loginToken: false
-      });
+      }); */
       return null;
       // 未登录
       // ElementUI.Message({ message: _data.viewMsg, type: 'error', customClass: 'request_tip' });
