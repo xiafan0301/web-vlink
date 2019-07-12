0f2e4080a27d799f6a027471867c3ecfa531063e
e <fen.wang@aorise.org>
2019-07-12 09:52:46 +0800

    频繁出没bug修复

--- src/config/config.js
+++ src/config/config.js
@@ -46,7 +46,7 @@ const oDomains = {
     // 研判检索服务
     judge: {
         // dev: 'http://10.116.64.142:8087/',
-        dev: 'https://apirel.aorise.org/vlink-judge/',
+        dev: 'https://apidev.aorise.org/vlink-judge/',
         test: 'https://apirel.aorise.org/vlink-judge/',
         prod: 'https://apirel.aorise.org/vlink-judge/',
     },
