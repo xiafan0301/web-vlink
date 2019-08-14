ec59ee76b63ea6f9f1ba7393391c95aea3196af1
longjiao.ouyang <longjiao.ouyang@aorise.org>
2019-08-14 11:13:57 +0800

    didi

--- src/config/config.js
+++ src/config/config.js
@@ -10,14 +10,14 @@ const oDomains = {
       dev: 'https://apidev.aorise.org/vlink-base',
       // dev: 'http://10.116.64.97:8081',//向
       test: 'https://apirel.aorise.org/vlink-base',
-      prod: 'https://apirel.aorise.org/vlink-base'
+      prod: 'https://apiprod.aorise.org/vlink-base'
     },
     // 视频服务地址
     video: {
         dev: 'https://apidev.aorise.org/vlink-monitor',
         // dev: 'http://10.116.64.82:8082',
         test: 'https://apirel.aorise.org/vlink-monitor',
-        prod: 'https://apirel.aorise.org/vlink-monitor',
+        prod: 'https://apiprod.aorise.org/vlink-monitor',
     },
     // 布控部分地址
     control: {
@@ -25,7 +25,7 @@ const oDomains = {
         // test: 'http://10.116.64.98:8081',//钟赞
         // dev: 'http://10.116.64.127:8081',//龙志海
         test: 'https://apirel.aorise.org/vlink-control',
-        prod: 'https://apirel.aorise.org/vlink-control'
+        prod: 'https://apiprod.aorise.org/vlink-control'
     },
     // 消息部分地址
     message: {
@@ -34,14 +34,14 @@ const oDomains = {
         // dev: 'http://10.116.64.142:8083',
         // dev: 'http://10.116.64.92:8081',//钟赞
         test: 'https://apirel.aorise.org/vlink-message',
-        prod: 'https://apirel.aorise.org/vlink-message'
+        prod: 'https://apiprod.aorise.org/vlink-message'
     },
     // 事件部分地址
     event: {
       dev: 'https://apidev.aorise.org/vlink-event',
       // dev: 'http://10.116.64.142:8081',
       test: 'https://apirel.aorise.org/vlink-event',
-      prod: 'https://apirel.aorise.org/vlink-event'
+      prod: 'https://apiprod.aorise.org/vlink-event'
     },
     // 研判检索服务
     judge: {
@@ -53,7 +53,7 @@ const oDomains = {
         // dev: 'https://apirel.aorise.org/vlink-judge/',
         test: 'https://apirel.aorise.org/vlink-judge/',
         // test: 'http://10.116.65.47:8081',
-        prod: 'https://apirel.aorise.org/vlink-judge/',
+        prod: 'https://apiprod.aorise.org/vlink-judge/',
     },
     // 调度和预案部分地址
     ctc: {
@@ -61,64 +61,64 @@ const oDomains = {
       // dev: 'http://10.116.64.142:8083/api/vlink',
       // dev: 'http://10.116.64.127:8081/api/vlink',
       test: 'https://apirel.aorise.org/vlink-dispatching/api/vlink',
-      prod: 'https://apirel.aorise.org/vlink-dispatching/api/vlink'
+      prod: 'https://apiprod.aorise.org/vlink-dispatching/api/vlink'
     },
     // 用户登录地址
     user: {
       // dev: 'http://10.116.64.107:8082', // 熊日成
       dev: 'https://apidev.aorise.org/vlink-user',
       test: 'https://apirel.aorise.org/vlink-user',
-      prod: 'https://apirel.aorise.org/vlink-user'
+      prod: 'https://apiprod.aorise.org/vlink-user'
     },
     // 用户权限地址
     auth: {
       // dev: 'http://10.116.64.107:8081/api/auth/', // 江先琨
       dev: 'https://apidev.aorise.org/vlink-auth/api/auth/',
       test: 'https://apirel.aorise.org/vlink-auth/api/auth/',
-      prod: 'https://apirel.aorise.org/vlink-auth/api/auth/'
+      prod: 'https://apiprod.aorise.org/vlink-auth/api/auth/'
     },
     // 车辆地址
     vehicle: {
       // dev: 'http://10.116.64.107:8081',
       dev: 'https://apidev.aorise.org/vlink-vehicle',
       test: 'https://apirel.aorise.org/vlink-vehicle',
-      prod: 'https://apirel.aorise.org/vlink-vehicle'
+      prod: 'https://apiprod.aorise.org/vlink-vehicle'
     },
     // 公共短信服务
     sms: {
       // dev: 'http://10.116.64.169:8085',
       dev: 'https://apidev.aorise.org/smart-city-common/',
       test: 'https://apirel.aorise.org/smart-city-common/',
-      prod: 'https://apidev.aorise.org/smart-city-common/'
+      prod: 'https://apiprod.aorise.org/smart-city-common/'
     },
     // 文件上传
     upload: {
         dev: 'https://apidev.aorise.org/vlink-base',
         test: 'https://apirel.aorise.org/vlink-base',
-        prod: 'https://apirel.aorise.org/vlink-base'
+        prod: 'https://apiprod.aorise.org/vlink-base'
     },
     // test-video服务
     live: {
       dev: 'https://apidev.aorise.org/vlink-link',
       test: 'https://apirel.aorise.org/vlink-link',
-      prod: 'https://apidev.aorise.org/vlink-link'
+      prod: 'https://apiprod.aorise.org/vlink-link'
     },
     // 视频播放服务 gis
     gis: {
       dev: 'https://apidev.aorise.org/vlink-gis',
       test: 'https://apirel.aorise.org/vlink-gis',
-      prod: 'https://apidev.aorise.org/vlink-gis'
+      prod: 'https://apiprod.aorise.org/vlink-gis'
     },
     //研判分析服务
     analysis: {
       dev: 'http://apidev.aorise.org/vlink-judge',
       test: 'http://10.116.65.15:8081',
-      prod: 'https://apidev.aorise.org/vlink-buz-judge-analysis'
+      prod: 'https://apiprod.aorise.org/vlink-buz-judge-analysis'
     },
     structuring: {
       dev: 'https://apidev.aorise.org/vlink-structuring',
       test: 'https://apirel.aorise.org/vlink-structuring',
-      prod: 'https://apirel.aorise.org/vlink-structuring',
+      prod: 'https://apiprod.aorise.org/vlink-structuring',
     }
 };
 let ENV_API = process.env.VUE_APP_API;
